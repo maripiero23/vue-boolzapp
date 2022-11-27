fcc198ada24fd6789aba546daa3cb41cb8cf04e7
maripiero23 <mari.pieropan@gmail.com>
2022-11-27 18:24:45 +0100

    manca setTimeout e Milestone 5

--- js/main.js
+++ js/main.js
@@ -1,100 +1,126 @@
-// Dall'oggetto globale Vue, estrai in una variabile la funzione "createApp"
-const { createApp } = Vue;
+const { createApp } = Vue
 
-// invochiamo la funzione passando come argomento un oggetto
+const app = createApp({
+  data () {
+    return {
+      usersList: [
+        {
+        name: 'Michele',
+        avatar: '_1',
+        visible: true,
+        messages: [{
+          date: '10/01/2020 15:30:55',
+          message: 'Hai portato a spasso il cane?',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 15:50:00',
+          message: 'Ricordati di dargli da mangiare',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 16:15:22',
+          message: 'Tutto fatto!',
+          status: 'received'
+        }
+        ],
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+        visible: true,
+        messages: [{
+          date: '20/03/2020 16:30:00',
+          message: 'Ciao come stai?',
+          status: 'sent'
+        },
+        {
+          date: '20/03/2020 16:30:55',
+          message: 'Bene grazie! Stasera ci vediamo?',
+          status: 'received'
+        },
+        {
+          date: '20/03/2020 16:35:00',
+          message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+          status: 'received'
+        }
+        ],
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        visible: true,
+        messages: [{
+          date: '28/03/2020 10:10:40',
+          message: 'La Marianna va in campagna',
+          status: 'received'
+        },
+        {
+          date: '28/03/2020 10:20:10',
+          message: 'Sicuro di non aver sbagliato chat?',
+          status: 'sent'
+        },
+        {
+          date: '28/03/2020 16:15:22',
+          message: 'Ah scusa!',
+          status: 'received'
+        }
+        ],
+      },
+      {
+        name: 'Luisa',
+        avatar: '_4',
+        visible: true,
+        messages: [{
+          date: '10/01/2020 15:30:55',
+          message: 'Lo sai che ha aperto una nuova pizzeria?',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 15:50:00',
+          message: 'Si, ma preferirei andare al cinema',
+          status: 'received'
+        }
+        ],
+      },
+      ],
 
-// crea l'istanza di Vue. Sull'istanza invochiamo la funzione
-// mount. Questa si aspetta come un argomento un selettore html valido
-createApp({
-    // data: function () {}
-    data() {
-        // la funzione data deve SEMPRE ritornare un oggetto
-        return {
-            utenti: [{
-                name: 'Michele',
-                avatar: 'imgs/avatar_1.jpg',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Hai portato a spasso il cane?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 16:15:22',
-                    message: 'Tutto fatto!',
-                    status: 'received'
-                }
-                ],
-            },
-            {
-                name: 'Fabio',
-                avatar: 'imgs/avatar_2.jpg',
-                visible: true,
-                messages: [{
-                    date: '20/03/2020 16:30:00',
-                    message: 'Ciao come stai?',
-                    status: 'sent'
-                },
-                {
-                    date: '20/03/2020 16:30:55',
-                    message: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
-                },
-                {
-                    date: '20/03/2020 16:35:00',
-                    message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'received'
-                }
-                ],
-            },
-            {
-                name: 'Samuele',
-                avatar: 'imgs/avatar_3.jpg',
-                visible: true,
-                messages: [{
-                    date: '28/03/2020 10:10:40',
-                    message: 'La Marianna va in campagna',
-                    status: 'received'
-                },
-                {
-                    date: '28/03/2020 10:20:10',
-                    message: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
-                },
-                {
-                    date: '28/03/2020 16:15:22',
-                    message: 'Ah scusa!',
-                    status: 'received'
-                }
-                ],
-            },
-            {
-                name: 'Luisa',
-                avatar: 'imgs/avatar_4.jpg',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                }
-                ],
-            },
-            ]
-        };
+      currentIndex: 0,
 
-    },
-    methods: {
-
+      newMessaseText:"",
+      
     }
+  },
+  methods: {
+    selectedUser(index){
+      this.currentIndex = index;
+    },
+    //Funzione per aggiungere un nuovo messaggio
+    addText(currentIndex){
+      this.usersList[currentIndex].messages.push({ 
+        date: new Date().toLocaleString,
+        message: this.newMessaseText,
+        status: 'sent'
 
-}).mount("#app")
+      }),
+      
+
+      // appena ivio il nuovo messaggio faccio un reset dell'input altrimenti
+      //il testo scritto rimarrebbe nell' input
+      this.newMessaseText = "";
+    },
+
+    //Cancellazione di un messaggio
+    // messageDelete(currentIndex){
+    //   const confirmed = confirm("Si sicuro di voler cancellare questo messaggio?");
+
+    //   if(confirmed){
+    //     this.usersList[currentIndex].messages.splice(currentIndex, 1),
+
+    //   }
+
+    // }
+    
+  }
+
+}).mount('#app');
